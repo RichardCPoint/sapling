6c6cfe73604db5ccf59a082d334d18ec029e22dc
Richard Collins <rcollins@point.com>
2026-01-07 13:59:13 -0800

    ISL: Show cloud icon on remote bookmarks

--- addons/components/Tag.tsx
+++ addons/components/Tag.tsx
@@ -8,6 +8,7 @@
 import type {ReactNode} from 'react';
 import type {ReactProps} from './utils';
 
+import {Icon} from './Icon';
 import * as stylex from '@stylexjs/stylex';
 
 const styles = stylex.create({
@@ -20,17 +21,48 @@ const styles = stylex.create({
     fontFamily: 'var(--font-family)',
     fontSize: '11px',
     lineHeight: '16px',
-
-    textOverflow: 'ellipsis',
     maxWidth: '150px',
+  },
+  flex: {
+    display: 'inline-flex',
+    gap: 'var(--halfpad)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    display: 'block',
+    flexShrink: 0,
+  },
+  text: {
+    textOverflow: 'ellipsis',
     whiteSpace: 'nowrap',
     overflow: 'hidden',
+    minWidth: 0,
   },
 });
 
 export function Tag({
   xstyle,
+  icon = null,
+  children,
   ...rest
-}: {children: ReactNode; xstyle?: stylex.StyleXStyles} & ReactProps<HTMLSpanElement>) {
-  return <span {...stylex.props(styles.tag, xstyle)} {...rest} />;
+}: {
+  children: ReactNode;
+  icon?: null | React.ComponentProps<typeof Icon>['icon'];
+  xstyle?: stylex.StyleXStyles;
+} & ReactProps<HTMLSpanElement>) {
+  if (icon != null) {
+    return (
+      <span {...stylex.props(styles.tag, styles.flex, xstyle)} {...rest}>
+        <Icon size="S" icon={icon} {...stylex.props(styles.icon)} />
+        <span {...stylex.props(styles.text)}>{children}</span>
+      </span>
+    );
+  } else {
+    return (
+      <span {...stylex.props(styles.tag, styles.text, xstyle)} {...rest}>
+        {children}
+      </span>
+    );
+  }
 }
